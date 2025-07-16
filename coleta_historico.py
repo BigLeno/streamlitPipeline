689335e26ac335c8ecaac69f8756e22047bbdb97
Rutileno Gabriel <rutileno.silva.702@ufrn.edu.br>
2025-07-16 08:44:14 -0300

    refactor: remove arquivos de coleta e análise de ativos

--- coleta_historico.py
+++
@@ -1,8 +0,0 @@
-
-
-from assets.config_assets import ATIVOS
-from assets.scrapping import Scraper
-
-scraper = Scraper(headless=True)
-
-scraper.coletar_e_salvar_historico_ativos(ATIVOS, periodos='5Y')
